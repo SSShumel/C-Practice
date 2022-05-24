2013b0eb46272f1da85ec75cffebec3ce8b6123b
Shumel Valery <shumel.valera24@gmail.com>
2022-05-24 21:09:06 +0300

    9 work

---
+++ app9/task1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+namespace Interface01
+{
+    interface Ix
+    {
+        void IxF0(double xKey);
+        void IxF1();
+    }
+    interface Iy
+    {
+        void F0(double xKey);
+        void F1();
+    }
+    interface Iz
+    {
+        void F0(double xKey);
+        void F1();
+    }
+    class TestClass : Ix, Iy, Iz
+    {
+        public double w;
+        public TestClass()
+        {
+            w = 125;
+        }
+        public TestClass(double key)
+        {
+            w = key;
+        }
+        public void IxF0(double w)
+        {
+            w = Math.Pow(w,2);
+            Console.WriteLine("IxF0({0})", w);
+        }
+    public void IxF1()
+        {
+            w = Math.Pow(w,2);
+            Console.WriteLine("IxF1({0})", w);
+        }
+    public void F0(double w)
+        {
+            w = Math.Sqrt(w);
+            Console.WriteLine("(Iy/Iz)F0({0})", w);
+        }
+     public void F1()
+        {
+            w = Math.Sqrt(w);
+            Console.WriteLine("(Iy/Iz)F1({0})", w);
+        }
+    void Iz.F0(double w)
+        {
+            w = Math.Pow(w,2) + 5;
+            Console.WriteLine("Iz.F0({0})", w);
+        }
+        void Iz.F1()
+        {
+            w = Math.Pow(w, 2) + 5;
+            Console.WriteLine("Iz.F1({0})", w);
+        }
+        
+}
+class Class1
+{
+    static void Main(string[] args)
+    {
+        TestClass x0 = new TestClass();
+        TestClass x1 = new TestClass(125);
+        x0.IxF0(10);
+        x1.IxF1();
+        x0.F0(5);
+        x1.F1();
+        (x0 as Iy).F0(7);
+        (x1 as Iz).F1();
+        
+        Console.WriteLine("==========Ix==========");
+        Ix ix = x1;
+        ix.IxF0(5);
+        ix.IxF1();
+    
+        Console.WriteLine("==========Iy==========");
+        Iy iy = x1;
+        iy.F0(5);
+        iy.F1();
+
+        Console.WriteLine("==========Iz==========");
+        Iz iz = x1;
+        iz.F0(5);
+        iz.F1();
+        
+    }
+}
+}
---
+++ app9/task2/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace task2
+{
+    class Array
+    {
+        public string[] lines;
+        public Array(int size)
+        {
+            lines = new string[size];
+        }
+        public void Filling()
+        {
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine("Введите строку");
+                lines[i] = Console.ReadLine();
+            }
+        }
+        public void Conclusion()
+        {
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.Write(lines[i]+" ");
+            }
+            Console.WriteLine();
+        }
+        public void Appeal(int index)
+        {
+            if ((index>lines.Length-1) || (index < 0))
+            {
+                Console.WriteLine("Индекс вышел за пределы массива");
+            }
+            else
+            {
+                Console.WriteLine(lines[index]);
+            }
+        }
+        public void AppealArray(int index)
+        {
+            Conclusion();
+            Appeal(index);
+        }
+        public static Array operator +(Array MyArray1, Array MyArray2)
+        {
+            string[] newLines = new string[Math.Max(MyArray1.lines.Length, MyArray2.lines.Length)];
+            for (int i = 0; i < newLines.Length; i++)
+            {
+                if (i > MyArray1.lines.Length - 1)
+                {
+                    newLines[i] = MyArray2.lines[i];
+                }
+                else if (i > MyArray2.lines.Length - 1)
+                {
+                    newLines[i] = MyArray1.lines[i];
+                }
+                else
+                {
+                    newLines[i] = MyArray1.lines[i] + MyArray2.lines[i];
+                }
+
+            }
+            Array newArray = new Array(newLines.Length);
+            newArray.lines = newLines;
+            return newArray;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array array1 = new Array(3);
+            array1.Filling();
+            Console.Write("1 массив: ");
+            array1.Conclusion();
+
+            Array array2 = new Array(2);
+            array2.Filling();
+            Console.Write("2 массив: ");
+            array2.Conclusion();
+
+            Array array3 = array1 + array2;
+            Console.Write("3 массив: ");
+            array3.Conclusion();
+            array3.Appeal(2);
+            array3.AppealArray(0);
+        }
+    }
+}
