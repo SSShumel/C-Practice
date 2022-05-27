332d2ccb079a547f89408743b61ae0efaf2e4c0c
Shumel Valery <shumel.valera24@gmail.com>
2022-05-27 20:12:45 +0300

    work

---
+++ app12/task2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        public delegate double MyDelegat(double a, double b);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите действие--> +,-,*,/");
+            string a = Console.ReadLine();
+            Console.WriteLine("Первое число -->");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Второе число -->");
+            double y = Convert.ToDouble(Console.ReadLine());
+            switch (a)
+            {
+                case "+":
+                    MyDelegat myDelegat = (c, b) => { return c + b; };
+                    double del = myDelegat(x, y);
+                    Console.WriteLine("Ответ:{0}", del);
+                    break;
+                case "-":
+                    MyDelegat myDelegat1 = (c, b) => { return c - b; };
+                    double del1 = myDelegat1(x, y);
+                    Console.WriteLine("Ответ:{0}", del1);
+                    break;
+                case "*":
+                    MyDelegat myDelegat2 = (c, b) => { return c * b; };
+                    double del2 = myDelegat2(x, y);
+                    Console.WriteLine("Ответ:{0}", del2);
+                    break;
+                case "/":
+                    MyDelegat myDelegat3 = (c, b) => {
+                        if ((c != 0) && (b != 0))
+                            return c / b;
+                        else
+                            Console.WriteLine("операция деления на 0 не допускается");
+                            return 0;
+                    };
+                    double del3 = myDelegat3(x, y);
+                    if (del3!=0)
+                        Console.WriteLine("Ответ:{0}", del3);
+                    break;
+                default:
+                    Console.WriteLine("Выберите из предложанного");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ app12/task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        public delegate string StringDelegate (string line);
+        static string Increase(string line)
+        {
+            Console.WriteLine($"строка с увеличенным регистром: {line.ToUpper()}");
+            return line.ToUpper();
+        }
+        static string TurnOver(string line)
+        {
+            char[] sReverse = line.ToCharArray();
+            Array.Reverse(sReverse);
+            line = new string(sReverse);
+            Console.WriteLine($"строка задом наперед: {line}");
+            return line;
+        }
+        static string Decrease(string line)
+        {
+            Console.WriteLine($"строка с уменьшенным регистром: {line.ToLower()}");
+            return line.ToLower();
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку:");
+            string line = Console.ReadLine();
+            StringDelegate SD = new StringDelegate(Increase);
+            SD += TurnOver;
+            SD += Decrease;
+            SD(line);
+        }
+    }
+}
