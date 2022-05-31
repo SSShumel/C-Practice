a115dd0433b0079fe4b3ebe5d5ac7d647a69ad86
Shumel Valery <shumel.valera24@gmail.com>
2022-05-31 21:22:36 +0300

    work

---
+++ app15/task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace task1
+{
+    class MyList<T>
+    {
+        protected int index = 0;
+        protected T[] Values = new T[0];
+        
+        public void Add(T value)
+        {
+            Array.Resize(ref Values, index+1);
+
+            Values[index] = value;
+            index++;
+        }
+        public T this[int index]
+        {
+            get => Values[index];
+        }
+        
+        public int Length
+        {
+            get => Values.Length ; 
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList <int> list = new MyList<int>();
+            list.Add(32994);
+            list.Add(200002);
+            list.Add(101);
+            Console.WriteLine("Элемент с индексом 1: " + list[1]);
+            Console.WriteLine("Длина: " + list.Length);
+        }
+    }
+}
---
+++ app15/task2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace task2
+{
+    class MyDictionary<TKey,TValue>
+    {
+        protected int index = 0;
+        protected TKey[] Keys = new TKey[0];
+        protected TValue[] Values = new TValue[0];
+
+        public void Add(TKey key, TValue value)
+        {
+            if (Array.IndexOf(Keys, key) == -1)
+            {
+                Array.Resize(ref Keys, index + 1);
+                Keys[index] = key;
+
+                Array.Resize(ref Values, index + 1);
+                Values[index] = value;
+
+                index++;
+            }
+            else { Console.WriteLine("Значение с таким ключем уже существуюе"); }
+        }
+        public TValue this[TKey index]
+        {
+            get => Values[Array.IndexOf(Keys,index)];
+        }
+
+        public int Length
+        {
+            get => Values.Length;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string,int> list = new MyDictionary<string, int>();
+            list.Add("lol",32994);
+            list.Add("popit",200002);
+            list.Add("shazam",101);
+            list.Add("popit", 5345353);
+            Console.WriteLine("Элемент с ключем popit: " + list["popit"]);
+            Console.WriteLine("Количество пар: " + list.Length);
+        }
+    }
+}
